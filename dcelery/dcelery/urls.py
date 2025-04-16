781c2b1de8726d93b7e69b56b0754b00cda41748
mrkataei <kouroshinfo96@gmail.com>
2025-04-17 01:21:41 +0330

    configs adde - new app started

--- dcelery/dcelery/urls.py
+++ dcelery/dcelery/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for celery project.
+URL configuration for dcelery project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
